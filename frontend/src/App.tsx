27422692564999065a9e9787d4d52dcf14468c20
Rahul1038402 <rahul1038402@gmail.com>
2025-11-26 13:11:27 +0530

    fix: changed the path of builder component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,15 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/hooks/use-theme";
-import ScrollToTop from "@/components/ScrollToTop"; // Add this import
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import Analyzer from "./pages/Analyzer";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import Builder_page from "./pages/Builder_page";
 import JobTracker from "./pages/JobTracker";
 import AboutMe from "./pages/AboutMe";
+import ATSComparisonPage from "./pages/ATSComparisonPage";
+import ResumeBuilder from "./ResumeBuilder";
 
 const queryClient = new QueryClient();
 
@@ -23,14 +24,15 @@ const App = () => {
           <Toaster />
           <Sonner />
           <BrowserRouter>
-            <ScrollToTop /> {/* Add this component inside BrowserRouter */}
+            <ScrollToTop />
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/analyzer" element={<Analyzer />} />
               <Route path="/about" element={<About />} />
               <Route path="/aboutme" element={<AboutMe />} />
-              <Route path="/builder" element={<Builder_page />} />
+              <Route path="/builder" element={<ResumeBuilder />} />
               <Route path="/tracker" element={<JobTracker />} />
+              <Route path="/ats" element={<ATSComparisonPage />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
