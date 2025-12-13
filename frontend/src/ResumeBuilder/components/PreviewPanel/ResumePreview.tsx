28dcd210b03dac76389520f0668f1c9f15335f45
Rahul1038402 <rahul1038402@gmail.com>
2025-12-13 22:46:32 +0530

    fix: updated rendering of project and experience section to not add blank spaces

--- frontend/src/ResumeBuilder/components/PreviewPanel/ResumePreview.tsx
+++ frontend/src/ResumeBuilder/components/PreviewPanel/ResumePreview.tsx
@@ -155,179 +155,202 @@ export const ResumePreview = ({ resumeData, layoutSettings }: ResumePreviewProps
             })()}
 
             {/* Experience */}
-            {resumeData.experience.length > 0 && (
-                <div style={{ marginBottom: '10pt' }}>
-                    <h2 style={{
-                        fontSize: '14.4pt',
-                        textTransform: 'uppercase',
-                        textAlign: 'left',
-                        fontWeight: 'normal',
-                        marginBottom: '0',
-                        paddingBottom: '1pt',
-                        borderBottom: '0.4pt solid black',
-                        marginTop: '10pt'
-                    }}>
-                        Experience
-                    </h2>
-                    <div style={{ marginTop: '6pt' }}>
-                        {resumeData.experience.map((exp) => (
-                            <div key={exp.id} style={{
-                                marginBottom: '9pt',
-                                fontWeight: 'normal'
-                            }}>
-                                <div style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'baseline',
-                                    marginBottom: '6pt',
-                                    marginTop: '10pt'
+            {resumeData.experience.length > 0 && (() => {
+                // Filter out empty experiences
+                const validExperiences = resumeData.experience.filter(exp =>
+                    exp.position.trim() ||
+                    exp.company.trim() ||
+                    exp.achievements.some(a => a.trim())
+                );
+
+                // Only render if there are valid experiences
+                if (validExperiences.length === 0) return null;
+
+                return (
+                    <div style={{ marginBottom: '10pt' }}>
+                        <h2 style={{
+                            fontSize: '14.4pt',
+                            textTransform: 'uppercase',
+                            textAlign: 'left',
+                            fontWeight: 'normal',
+                            marginBottom: '0',
+                            paddingBottom: '1pt',
+                            borderBottom: '0.4pt solid black',
+                            marginTop: '10pt'
+                        }}>
+                            Experience
+                        </h2>
+                        <div style={{ marginTop: '6pt' }}>
+                            {validExperiences.map((exp) => (
+                                <div key={exp.id} style={{
+                                    marginBottom: '9pt',
+                                    fontWeight: 'normal'
                                 }}>
                                     <div style={{
-                                        fontSize: '12pt',
-                                        fontWeight: 'normal'
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'baseline',
+                                        marginBottom: '6pt',
+                                        marginTop: '10pt'
                                     }}>
-                                        <span style={{ fontWeight: 'bold' }}>{exp.position}</span>
-
-                                        {exp.company && (
-                                            <>
-                                                <span> at </span>
-                                                <span style={{ fontWeight: 'bold' }}>{exp.company}</span>
-                                            </>
-                                        )}
-
-                                        {exp.location && (
-                                            <span style={{ marginLeft: '4pt' }}>
-                                                ({exp.location})
-                                            </span>
-                                        )}
-
-                                    </div>
-                                    <span style={{
-                                        fontSize: '12pt',
-                                        fontWeight: 'normal'
-                                    }}>
-                                        {exp.startDate} - {exp.endDate}
-                                    </span>
-                                </div>
-                                {exp.achievements.length > 0 && exp.achievements[0] && (
-                                    <ul style={{
-                                        fontSize: '10pt',
-                                        marginTop: '2pt',
-                                        marginBottom: '0',
-                                        marginLeft: '1em',
-                                        paddingLeft: '0',
-                                        listStyleType: 'none',
-                                        fontWeight: 'normal'
-                                    }}>
-                                        {exp.achievements.map((achievement, index) => (
-                                            achievement && <li key={index} style={{
-                                                lineHeight: '1.3',
-                                                marginBottom: '3pt',
-                                                position: 'relative',
-                                                paddingLeft: '1.2em'
-                                            }}>
-                                                <span style={{
-                                                    position: 'absolute',
-                                                    left: '0'
-                                                }}>•</span>
-                                                {achievement}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )}
-
-            {/* Projects */}
-            {resumeData.projects.length > 0 && (
-                <div style={{ marginBottom: '10pt' }}>
-                    <h2 style={{
-                        fontSize: '14.4pt',
-                        textTransform: 'uppercase',
-                        textAlign: 'left',
-                        fontWeight: 'normal',
-                        marginBottom: '0',
-                        paddingBottom: '1pt',
-                        borderBottom: '0.4pt solid black',
-                        marginTop: '10pt'
-                    }}>
-                        Projects
-                    </h2>
-                    <div style={{ marginTop: '6pt' }}>
-                        {resumeData.projects.map((project) => (
-                            <div key={project.id} style={{
-                                marginBottom: '9pt',
-                                fontWeight: 'normal'
-                            }}>
-                                <div style={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'baseline',
-                                    marginBottom: '2pt'
-                                }}>
-                                    <h3 style={{
-                                        fontSize: '12pt',
-                                        fontWeight: 'bold',
-                                        margin: '0'
-                                    }}>
-                                        {project.title}
-                                    </h3>
-                                    {project.link && (
-                                        <a href={project.link} target='_blank' style={{
-                                            color: 'rgb(0, 51, 153)',
-                                            textDecoration: 'none',
+                                        <div style={{
                                             fontSize: '12pt',
                                             fontWeight: 'normal'
                                         }}>
-                                            Live Demo
-                                        </a>
+                                            <span style={{ fontWeight: 'bold' }}>{exp.position}</span>
+
+                                            {exp.company && (
+                                                <>
+                                                    <span> at </span>
+                                                    <span style={{ fontWeight: 'bold' }}>{exp.company}</span>
+                                                </>
+                                            )}
+
+                                            {exp.location && (
+                                                <span style={{ marginLeft: '4pt' }}>
+                                                    ({exp.location})
+                                                </span>
+                                            )}
+                                        </div>
+                                        <span style={{
+                                            fontSize: '12pt',
+                                            fontWeight: 'normal'
+                                        }}>
+                                            {exp.startDate} - {exp.endDate}
+                                        </span>
+                                    </div>
+                                    {exp.achievements.length > 0 && exp.achievements[0] && (
+                                        <ul style={{
+                                            fontSize: '10pt',
+                                            marginTop: '2pt',
+                                            marginBottom: '0',
+                                            marginLeft: '1em',
+                                            paddingLeft: '0',
+                                            listStyleType: 'none',
+                                            fontWeight: 'normal'
+                                        }}>
+                                            {exp.achievements.map((achievement, index) => (
+                                                achievement && <li key={index} style={{
+                                                    lineHeight: '1.3',
+                                                    marginBottom: '3pt',
+                                                    position: 'relative',
+                                                    paddingLeft: '1.2em'
+                                                }}>
+                                                    <span style={{
+                                                        position: 'absolute',
+                                                        left: '0'
+                                                    }}>•</span>
+                                                    {achievement}
+                                                </li>
+                                            ))}
+                                        </ul>
                                     )}
                                 </div>
-                                {project.technologies && (
-                                    <p style={{
-                                        fontSize: '9pt',
-                                        fontStyle: 'italic',
-                                        marginBottom: '2pt',
-                                        marginTop: '0',
-                                        fontWeight: 'normal'
-                                    }}>
-                                        Tech Stack: {project.technologies}
-                                    </p>
-                                )}
-                                {project.description.length > 0 && project.description[0] && (
-                                    <ul style={{
-                                        fontSize: '10pt',
-                                        marginTop: '2pt',
-                                        marginBottom: '0',
-                                        marginLeft: '1em',
-                                        paddingLeft: '0',
-                                        listStyleType: 'none',
-                                        fontWeight: 'normal'
-                                    }}>
-                                        {project.description.map((desc, index) => (
-                                            desc && <li key={index} style={{
-                                                lineHeight: '1.3',
-                                                marginBottom: '3pt',
-                                                position: 'relative',
-                                                paddingLeft: '1.2em'
-                                            }}>
-                                                <span style={{
-                                                    position: 'absolute',
-                                                    left: '0'
-                                                }}>•</span>
-                                                {desc}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
+                );
+            })()}
+
+            {/* Projects */}
+            {resumeData.projects.length > 0 && (() => {
+                // Filter out empty projects
+                const validProjects = resumeData.projects.filter(project =>
+                    project.title.trim() ||
+                    project.technologies.trim() ||
+                    project.description.some(d => d.trim())
+                );
+
+                // Only render if there are valid projects
+                if (validProjects.length === 0) return null;
+
+                return (
+                    <div style={{ marginBottom: '10pt' }}>
+                        <h2 style={{
+                            fontSize: '14.4pt',
+                            textTransform: 'uppercase',
+                            textAlign: 'left',
+                            fontWeight: 'normal',
+                            marginBottom: '0',
+                            paddingBottom: '1pt',
+                            borderBottom: '0.4pt solid black',
+                            marginTop: '10pt'
+                        }}>
+                            Projects
+                        </h2>
+                        <div style={{ marginTop: '6pt' }}>
+                            {validProjects.map((project) => (
+                                <div key={project.id} style={{
+                                    marginBottom: '9pt',
+                                    fontWeight: 'normal'
+                                }}>
+                                    <div style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'baseline',
+                                        marginBottom: '2pt'
+                                    }}>
+                                        <h3 style={{
+                                            fontSize: '12pt',
+                                            fontWeight: 'bold',
+                                            margin: '0'
+                                        }}>
+                                            {project.title}
+                                        </h3>
+                                        {project.link && (
+                                            <a href={project.link} target='_blank' style={{
+                                                color: 'rgb(0, 51, 153)',
+                                                textDecoration: 'none',
+                                                fontSize: '12pt',
+                                                fontWeight: 'normal'
+                                            }}>
+                                                Live Demo
+                                            </a>
+                                        )}
+                                    </div>
+                                    {project.technologies && (
+                                        <p style={{
+                                            fontSize: '9pt',
+                                            fontStyle: 'italic',
+                                            marginBottom: '2pt',
+                                            marginTop: '0',
+                                            fontWeight: 'normal'
+                                        }}>
+                                            Tech Stack: {project.technologies}
+                                        </p>
+                                    )}
+                                    {project.description.length > 0 && project.description[0] && (
+                                        <ul style={{
+                                            fontSize: '10pt',
+                                            marginTop: '2pt',
+                                            marginBottom: '0',
+                                            marginLeft: '1em',
+                                            paddingLeft: '0',
+                                            listStyleType: 'none',
+                                            fontWeight: 'normal'
+                                        }}>
+                                            {project.description.map((desc, index) => (
+                                                desc && <li key={index} style={{
+                                                    lineHeight: '1.3',
+                                                    marginBottom: '3pt',
+                                                    position: 'relative',
+                                                    paddingLeft: '1.2em'
+                                                }}>
+                                                    <span style={{
+                                                        position: 'absolute',
+                                                        left: '0'
+                                                    }}>•</span>
+                                                    {desc}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                );
+            })()}
 
             {/* Education */}
             {resumeData.education.length > 0 && (
