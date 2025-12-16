d3c20764cbd1b27620e70a7c3f3531bbc9da1ad6
Rahul1038402 <rahul1038402@gmail.com>
2025-12-16 23:12:09 +0530

    fix: aligned top buttons from new navbar

--- frontend/src/ResumeBuilder/components/TopControls.tsx
+++ frontend/src/ResumeBuilder/components/TopControls.tsx
@@ -19,8 +19,12 @@ export const TopControls = ({
     downloadProgress,
 }: TopControlsProps) => {
     return (
-        <div className="bg-white dark:bg-black shadow-md top-16 z-30">
-            <div className="container mx-auto px-4 pb-2">
+        <div className="bg-transparent top-16 z-30">
+            <div className="relative overflow-hidden container mx-auto px-4 pb-5">
+                                  <div
+        className="absolute shadow-lg bottom-0 left-0 right-0 h-px bg-[linear-gradient(to_right,transparent_0%,rgba(203,213,225,0.5)_20%,rgba(203,213,225,1)_50%,rgba(203,213,225,0.5)_80%,transparent_100%)]
+    dark:bg-[linear-gradient(to_right,transparent_0%,rgba(100,116,139,0.3)_20%,rgba(100,116,139,0.6)_50%,rgba(100,116,139,0.3)_80%,transparent_100%)]"
+      />
                 <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
                     {/* Left: Tab Buttons */}
                     <div className="flex gap-2 flex-wrap justify-center">
